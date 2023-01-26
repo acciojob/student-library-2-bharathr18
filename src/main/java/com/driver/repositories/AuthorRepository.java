244c84a5716ce8b34e064c164aa9e362d5acb3b4
bharathr18 <bharathr.16ec@saividya.ac.in>
2023-01-26 21:39:17 +0530

    lms2o

--- src/main/java/com/driver/repositories/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.driver.repositories;
-
-import com.example.library.studentlibrary.models.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Integer> {
-}
