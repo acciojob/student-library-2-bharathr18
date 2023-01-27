30b836598f23eb91f4166b447fd3d2ed24ae86d5
bharathr18 <bharathr.16ec@saividya.ac.in>
2023-01-27 11:00:55 +0530

    lms2

---
+++ src/test/java/com/driver/services/AuthorServiceTest.java
@@ -0,0 +1,22 @@
+package com.driver.services;
+
+import com.driver.repositories.AuthorRepository;
+import com.driver.models.Author;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AuthorServiceTest {
+    @InjectMocks AuthorService authorService;
+
+    @Mock
+    AuthorRepository authorRepository;
+
+    @Test
+    public void testCreate(){
+        authorService.create(new Author("1", "1@gmail.com", 21, "India"));
+    }
+}
---
+++ src/test/java/com/driver/services/CardServiceTest.java
@@ -0,0 +1,77 @@
+package com.driver.services;
+
+import com.driver.repositories.CardRepository;
+import com.driver.models.Author;
+import com.driver.models.Card;
+import com.driver.models.Student;
+import com.driver.repositories.AuthorRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static com.driver.models.CardStatus.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CardServiceTest {
+    @InjectMocks CardService cardService;
+
+    @Mock
+    CardRepository cardRepository;
+
+
+    @InjectMocks AuthorService authorService;
+
+    @Mock
+    AuthorRepository authorRepository;
+
+//    @InjectMocks StudentService studentService;
+//
+//    @Mock
+//    CardService cardService;
+//
+//
+//    @Mock
+//    StudentRepository studentRepository;
+//
+//    List<Student> students = new ArrayList<>();
+
+
+    Student student = new Student();
+    Card card = new Card();
+    @Before
+    public void setUp(){
+        student.setAge(23);
+        student.setCountry("INDIA");
+        student.setName("Accio");
+        student.setEmailId("accio@gmail.com");
+        doAnswer((test) -> {
+            card.setCardStatus(DEACTIVATED);
+            return null;
+        }).when(cardRepository).deactivateCard(student.getId(),
+                DEACTIVATED.toString());
+
+    }
+
+    @Test
+    public void testCreateAndReturn(){
+        Card card = cardService.createAndReturn(student);
+        assert(card.getCardStatus() == ACTIVATED);
+        assert(card.getStudent() == student);
+    }
+
+    @Test
+    public void testDeactivateCard(){
+        cardService.deactivateCard(student.getId());
+        assert(card.getCardStatus() == DEACTIVATED);
+    }
+
+    @Test
+    public void testCreate(){
+        authorService.create(new Author("1", "1@gmail.com", 21, "India"));
+    }
+
+}
