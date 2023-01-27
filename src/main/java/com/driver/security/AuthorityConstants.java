2d57828fd1c8838218d9d13114d720b53757f5fe
bharathr18 <bharathr.16ec@saividya.ac.in>
2023-01-27 11:15:00 +0530

    lms2

--- src/main/java/com/driver/security/AuthorityConstants.java
+++
@@ -1,8 +0,0 @@
-package com.driver.security;
-
-public class AuthorityConstants {
-
-    public static final String ADMIN_AUTHORITY = "ADMIN";
-    public static final String STUDENT_AUTHORITY = "STUDENT";
-
-}
